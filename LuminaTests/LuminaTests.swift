b8d7aa3fdbedf75d7c6121e9f4d142e612e5c8e5
Stefan Engel <stefan.engel@dm.de>
2020-02-04 17:56:34 +0100

    Initial Commit

---
+++ LuminaTests/LuminaTests.swift
@@ -0,0 +1,34 @@
+//
+//  LuminaTests.swift
+//  LuminaTests
+//
+//  Created by Engel, Stefan on 04.02.20.
+//  Copyright © 2020 Stefan Engel Software Development. All rights reserved.
+//
+
+import XCTest
+@testable import Lumina
+
+class LuminaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
