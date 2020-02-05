dddbbf2d9310348ba2fed3e3def45905503e9c0c
Ehlen, David <david.ehlen@dm.de>
2020-02-05 17:35:00 +0100

    use the url components API to make sure we generate a valid URL

--- BuildStatusChecker/Sources/BuildStatusChecker/API/Bitrise/BitriseAPI.swift
+++ BuildStatusChecker/Sources/BuildStatusChecker/API/Bitrise/BitriseAPI.swift
@@ -30,11 +30,24 @@ extension BitriseAPI {
 // MARK: - Fetching builds
 extension BitriseAPI {
     static func builds(config: BitriseConfiguration, forBranch branch: String? = nil, limit: Int = 50) -> AnyPublisher<BitriseBuilds, Error> {
-        var query = "builds?workflow=primary&limit=\(limit)"
+        var baseURL = URL(string: config.baseUrl)!
+        baseURL.appendPathComponent(config.appSlug)
+        baseURL.appendPathComponent("builds")
+        
+        var queryItems = [
+            URLQueryItem(name: "workflow", value: "primary"),
+            URLQueryItem(name: "limit", value: "\(limit)"),
+        ]
+        
         if let branch = branch {
-            query.append("&branch=\(branch)")
+            queryItems.append(URLQueryItem(name: "branch", value: branch))
         }
-        let url = URL(string: "\(config.baseUrl)/\(config.appSlug)/\(query)")!
+        
+        var components = URLComponents(url: baseURL, resolvingAgainstBaseURL: false)!
+        components.queryItems = queryItems
+
+        let url = components.url!
+        
         var urlRequest = URLRequest(url: url)
         urlRequest.setValue(config.authToken, forHTTPHeaderField: "Authorization")
 
